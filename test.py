c03ed34d9078a27788440125620c478d62996024
Pandada8 <pandada8@gmail.com>
2015-06-20 16:46:28 +0800

    Finish loop

--- test.py
+++ test.py
@@ -1,69 +1,120 @@
 import asyncio
 import aiohttp
 
+
 class WrongCallError(Exception):
 
     def __repr__(self):
         return "You should only use .init() once"
 
 
+class End_Of_Life():
+    
+    def __repr__(self):
+        return "[We died]"
+
+EOL = End_Of_Life()
+
+
+def iterable(obj):
+    try:
+        iter(obj)
+        return True
+    except TypeError:
+        return False
+
+
 class Job(object):
 
     def __init__(self):
-        self.prev_func = None
-        self.prev_args = {}
+        self.funcs = []
+        self.loop = asyncio.get_event_loop()
+        self.prev_queue = None
 
     def init(self, func, **kwargs):
-        if self.prev_func is None:
-            self.prev_func = asyncio.coroutine(func)
-            self.prev_args = kwargs if kwargs else {}
+        if len(self.funcs) == 0:
+            queue = asyncio.Queue()
 
-    def then(self, func, **kwargs):
-        if self.prev_func is None:
+            def wrapper():
+                result = yield from asyncio.coroutine(func)(**kwargs)
+                if not iterable(result):
+                    result = [result]
+                for i in result:
+                    yield from queue.put(i)
+                yield from queue.put(EOL)  # we finished
+                print('Goodbye', func.__name__)
+
+            self.funcs.append(asyncio.coroutine(wrapper))
+            self.prev_queue = queue
+
+            return self
+        else:
             raise WrongCallError
 
-        # we should use clousure rather the self.* in the next function, which would cause a recursion calling
-        prev_func = self.prev_func
-        prev_args = self.prev_args
-        func = asyncio.coroutine(func)
-        buffer = []  # a simple buffer here to store the data
-        # @asyncio.coroutine
-        def wrapper():
-            result = yield from prev_func(**prev_args)
-            print(result)
-            if not asyncio.iscoroutine(result):
-                try:
-                    iter(result)
-                    a = list((yield from func(i, **kwargs)) for i in result)
-                    return a
-                except TypeError:
-                    return (yield from func(result, **kwargs))
+    def then(self, func, **kwargs):
+        if len(self.funcs) == 0:
+            raise WrongCallError
 
-        self.prev_args = kwargs if kwargs else {}
-        self.prev_func = wrapper
+        prev_queue = self.prev_queue
+        queue = asyncio.Queue()
+        func = asyncio.coroutine(func)
+
+        @asyncio.coroutine
+        def wrapper():
+            while True:
+                print(prev_queue.empty())
+                to_do = yield from prev_queue.get()
+                print('recv', to_do)
+                if to_do is not EOL:
+                    result = yield from func(to_do, **kwargs)
+                    print('result of ', func.__name__, result)
+                    if not iterable(result):
+                        result = [result]
+                    for i in result:
+                        print(i)
+                        yield from queue.put(i)
+                    # yield from queue.put(EOL)
+                    # print('Goodbye')
+                    # prev_queue.task_done()
+                else:
+                    yield from queue.put(EOL)
+                    return
+
+        self.prev_queue = queue
+        self.funcs.append(asyncio.coroutine(wrapper))
+        return self
+
+    def until_finish(self):
+        while True:
+            value = yield from self.prev_queue.get()
+            # self.prev_queue.task_done()
+            if value is EOL:
+                return
 
     def run(self):
+        tasks = []
+        for i in self.funcs:
+            tasks.append(self.loop.create_task(i()))
+        self.loop.run_forever()
+        # self.loop.run_until_complete(self.until_finish())
+        # self.loop.run_until_complete(asyncio.wait([asyncio.async(i) for i in tasks]))
 
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.prev_func(**self.prev_args))
-        loop.close()
+        self.loop.close()
 
 
 def step1():
     print('Step:1')
     return list(range(10))
 
+
 def step2(x):
-    print('step2')
-    return x *x
+    print('step:2')
+    return x * x
+
 
 def step3(x):
     print("Step:3")
     return [x, x]
 
 job = Job()
-job.init(step1)
-job.then(step2)
-job.then(step3)
-job.then(print)
-job.run()
+job.init(step1).then(step2).then(step3).run()
